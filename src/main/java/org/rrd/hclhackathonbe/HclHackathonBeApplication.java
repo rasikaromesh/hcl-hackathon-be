1fc5febd7b644351f449187cce274bbf7078fce1
rasikaromesh <rasikaromesh000@gmail.com>
2024-04-27 10:31:03 +0800

    first commit

---
+++ src/main/java/org/rrd/hclhackathonbe/HclHackathonBeApplication.java
@@ -0,0 +1,13 @@
+package org.rrd.hclhackathonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HclHackathonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HclHackathonBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rrd/hclhackathonbe/HclHackathonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rrd.hclhackathonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HclHackathonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
